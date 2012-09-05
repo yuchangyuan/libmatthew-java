b57623452d4f821fd29da43656904b8bf050e94f
Yu Changyuan <reivzy@gmail.com>
2012-09-05 18:43:27 +0800

    Ignore generated files during compile.

--- cx/ath/matthew/debug/Debug.java
+++ cx/ath/matthew/debug/Debug.java
@@ -2,7 +2,7 @@
  * Java Debug Library
  *
  * Copyright (c) Matthew Johnson 2005
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * 
+ *
  * To Contact the author, please email src@matthew.ath.cx
  *
  */
